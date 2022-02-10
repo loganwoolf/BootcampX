dedfba75717d61c2ed17c51bc8b3326924275bf1
Logan Woolf <loganwoolf@gmail.com>
2022-02-10 06:31:57 +0000

    Mitigate SQL injection using parameterized query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,19 +8,22 @@ const pool = new Pool({
   database: process.env.PG_DB
 });
 
-const cohort = process.argv[2];
-const limit = process.argv[3];
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
 
-pool.query(`
-SELECT
-  students.id AS id,
-  students.name AS name,
-  cohorts.name AS cohort
-FROM students
-JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name LIKE '${cohort}%'
-LIMIT ${limit}
-`)
+const queryString = `
+  SELECT
+    students.id AS id,
+    students.name AS name,
+    cohorts.name AS cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = students.cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2
+`;
+
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an ID of ${user.id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,17 +9,19 @@ const pool = new Pool({
 });
 
 const cohort = process.argv[2];
+const values = [cohort];
+const queryString = `
+  SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+  FROM teachers
+  JOIN assistance_requests ON teacher_id = teachers.id
+  JOIN students ON students.id = student_id
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name = $1
+  ORDER BY teacher;
+`;
 
 pool
-  .query(`
-    SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
-    FROM teachers
-    JOIN assistance_requests ON teacher_id = teachers.id
-    JOIN students ON students.id = student_id
-    JOIN cohorts ON cohorts.id = cohort_id
-    WHERE cohorts.name = '${cohort}'
-    ORDER BY teacher;
-  `)
+  .query(queryString, values)
   .then(res => {
     res.rows.forEach(entry => {
       console.log(`${entry.cohort}: ${entry.teacher}`);
